d3c0fc586d3cf5e568e4ab2e6e79ab20ffe3593c
Edvin Gavell <eddi@live.se>
2020-12-04 14:00:31 +0100

    updated file

--- src/Lab1/SNTPMessage.java
+++ src/Lab1/SNTPMessage.java
@@ -66,7 +66,7 @@ public class SNTPMessage {
     private double rootDispersion = 0;
     /*
     rootDelay 32-bit signed fixed-point number
-    fraction point between 14 and 16
+    fraction point between 15 and 16
 
     0000 0000 0000 0000 . 0000 0000 0000 0000
 
@@ -92,7 +92,6 @@ public class SNTPMessage {
           0 0   1 0 0   1 0 0 - (00100100) byte kod
            0      4       4
         */
-
         leapIndicator = (byte) ((b>>6) & 0x3);
         //00100100 -> 0010 0100
         // >> skiftar alla bits 6 steg till höger
@@ -107,39 +106,67 @@ public class SNTPMessage {
         // & 0x7?
         //0000 0100 -> 0000 0111
         mode = (byte) (b & 0x7); //0010 0100
-        stratum = buf[1]; // 1
-        pollInterval = buf[2]; // 0
+        stratum = unsignedByteToShort(buf[1]); // 1
+        pollInterval = unsignedByteToShort(buf[2]); // 0
         precision = buf[3]; // -25
-        rootDelay = buf[4] + buf[5] + buf[6] + buf[7]; // 0,   0,  0,   0,
-        rootDispersion = buf[8] + buf[9] + buf[10] + buf[11]; // 0,   0,  0,   2,
+
+        //Vi får datan för rootDelay som 4 bytes d.v.s 32 bits i följd
+        //00000000 00000000 00000000 00000000 | 00000000 00000000 00000000 00000000 -> 0 - 65535 | 0 - 65535 -> 0 - 65535,65535
+        //Exempel: 1000 0100 0110 0010 | 0110 0100 0110 0010 -> 33890.25698
+        // buf[4] = 132
+        // buf[5] = 98
+        // buf[6] = 100 (1000 0000)
+        // buf[7] = 98
+        // 256.0 -> 0xFF+1.0
+        // 65536.0 -> 0xFFFF+1.0
+        rootDelay = (((buf[4] * 256.0) +
+                      (unsignedByteToShort(buf[5]))) +
+                     ((unsignedByteToShort(buf[6]) / (0xFF+1.0)) +
+                      (unsignedByteToShort(buf[7]) / (0xFFFF+1.0)))); // 0,   0,  0,   0,
+
+        rootDispersion = (((buf[8] * 255.0) +
+                           (unsignedByteToShort(buf[9]))) +
+                          ((unsignedByteToShort(buf[10]) / (0xFF+1.0)) +
+                           (unsignedByteToShort(buf[11]) / (0xFFFF+1.0)))); // 0,   0,  0,   2,
+
+        // 0101 0000 | 0101 0000 | 0101 0011 | 0000 0000
+        //     80          80         83          0
+        //ASCII 80, 80, 83, 0 -> PPS
         referenceIdentifier[0] = buf[12]; // referenceIdentifier = 80, 80, 83, 0,
         referenceIdentifier[1] = buf[13];
         referenceIdentifier[2] = buf[14];
         referenceIdentifier[3] = buf[15];
 
-        referenceTimeStamp = buf[16] + buf[17] + buf[18] + buf[19] + buf[20] + buf[21] + buf[22] + buf[23]; // -29, 116,  5,  61,  0,  0,    0,   0,
-        originateTimeStamp = buf[24] + buf[25] + buf[26] + buf[27] + buf[28] + buf[29] + buf[30] + buf[31]; // -29, 116,  5,  59, 14, 86,    0,   0,
-        receiveTimeStamp = buf[32] + buf[33] + buf[34] + buf[35] + buf[36] + buf[37] + buf[38] + buf[39];   // -29, 116,  5,  62,  0, 47, -121, -38,
-        transmitTimeStamp = buf[40] + buf[41] + buf[42] + buf[43] + buf[44] + buf[45] + buf[46] + buf[47];  // -29, 116,  5,  62,  0, 47, -113,  -1}
-        /* Reference TimeStamp, Originate TimeStamp, Receive TimeStamp, Transmit TimeStamp:
-        those fields of 64 bits contain the four informations of time in connection with the “delays” and “the offset”.
-        The first 32 bits indicate the number of seconds passed since January 1, 1900 to 0h00 UTC.
-        The last 32 bits indicate the 1/232 seconds number spent since the beginning of the current second.
-        In Broadcast mode, the fields Originate TimeStamp and Receive TimeStamp are by convention set to zero.
-        TimeStamp reference: time of the last update of the clock
-        Originate TimeStamp: time of departure of the request.
-        Receive TimeStamp: time of arrival of the request
-        TimeStamp transmitted: time of departure of the response to the request.
-        Authenticator (optional): this fields is kept for the mechanism of authentication of NTP messages.
-        This mechanism authorizes the encoding of NTP data with the DES (Data Encryption Standard).
-        It is packed up in the appendix C of the RFC 1305.
-         */
 
+        //Timestamps is 64bits... instead of 32 we did earlier but still the same practice.
+        referenceTimeStamp = byteArrayToDouble(buf, 16); // -29, 116,  5,  61,  0,  0,    0,   0,
+        originateTimeStamp = byteArrayToDouble(buf, 24); // -29, 116,  5,  61,  0,  0,    0,   0,
+        receiveTimeStamp = byteArrayToDouble(buf, 32); // -29, 116,  5,  62,  0, 47, -121, -38,
+        transmitTimeStamp = byteArrayToDouble(buf, 40); // -29, 116,  5,  62,  0, 47, -113,  -1}
+    }
 
+    private double byteArrayToDouble(byte[] buf, int index) {
+        double result = 0.0;
+        for(int i = 0; i < 8; i++) {
+            result += unsignedByteToShort(buf[index + i]) * Math.pow(2, (3-i) * 8);
+        }
+        return result;
+    }
 
-        0001 0000
+    private short unsignedByteToShort(byte b) {
+        //signed byte tal mellan -126 - 127
+        //unsigned byte tal mellan 0 - 255
+        //Exempel b = 1101 1001, översta biten är satt och java tolkar som ett negativt tal
+        //Kolla om översta biten är satt genom bitvis and med 0x80 eller 1000 0000
 
-           128 64 32 16     8 4 2 1
-            0   0  0  0     0 0 1 1
+        if (((b>>7) & 0x1) == 1) {
+            return (short) ((b & 0x7F) + 128); // 0111 1111 -> 64+32+16+8+4+2+1 = 127 -> 0x7F hexadecimal, 128 tack vare första biten
+        } else {
+            return (short) ((b & 0xFF));
+        }
+
+        // if ((b & 0x80) == 0x80) { // 0x80 = 1000 0000
+        //    return (short) ((b & 0x7F)*2);
+        // }
     }
 }
